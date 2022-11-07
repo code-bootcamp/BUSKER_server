5006e2096571a063b872be5f053ff052309b95f5
= <=>
2022-11-07 16:33:29 +0900

    FIX: 환경변수 오타 수정
    FIX: 환경변수 오타 수정
    
    resovle #97

--- src/commons/jwt-social-kakao.ts
+++ src/commons/jwt-social-kakao.ts
@@ -4,8 +4,8 @@ import { Strategy } from 'passport-kakao';
 export class JwtKakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
   constructor() {
     super({
-      clientID: process.env.KAKAO_CLIEN_ID,
-      clientSecret: process.env.KAKAO_CLIEN_SECRET,
+      clientID: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/login/kakao',
       scope: ['account_email', 'profile_nickname'],
     });
