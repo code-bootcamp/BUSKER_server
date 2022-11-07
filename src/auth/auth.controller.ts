a9309910bdf083a2bc3e65e4e2e9f613a67a54e4
PackChanHyo <cksgy1510@gmail.com>
2022-11-07 15:41:16 +0900

    ADD: facebook-social-login
    close #79

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,4 +31,12 @@ export class AuthController {
   ) {
     this.authService.buskerSocialLogin({ req, res });
   }
+  @Get('/login/facebook')
+  @UseGuards(AuthGuard('facebook'))
+  async loginFacebook(
+    @Req() req: Request & IOAuthUser, //
+    @Res() res: Response,
+  ) {
+    this.authService.buskerSocialLogin({ req, res });
+  }
 }
