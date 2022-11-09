7538cecb95c1feb5bdcffe285a6f3edc6c26ff76
= <=>
2022-11-09 17:08:14 +0900

    fix-error

--- src/apis/artists/artists.service.ts
+++ src/apis/artists/artists.service.ts
@@ -1,4 +1,3 @@
-import { ArtistImage } from 'src/apis/artistImage/entity/artistImage.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserAuthority } from 'src/commons/role/entity/userAuthority.entity';
@@ -12,8 +11,6 @@ export class ArtistsService {
     private readonly artistRepository: Repository<Artist>,
     @InjectRepository(UserAuthority)
     private readonly userAuthorityRepository: Repository<UserAuthority>,
-    @InjectRepository(ArtistImage)
-    private readonly artistImageRepository: Repository<ArtistImage>,
   ) {}
 
   async findOneWithActiveName({ active_name }) {
