96f97f63f5727b04f925022d3756b1c54f4ff0ac
Aaron19965 <lyu19965@gmail.com>
2022-11-10 15:52:24 +0900

    ADD: updateUserImage
    close #144

--- src/apis/userImage/userImage.service.ts
+++ src/apis/userImage/userImage.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserImageInput } from './dto/createUserImage.input';
+import { UpdateUserImageInput } from './dto/updateUserImage.input';
 
 @Injectable()
 export class UserImageService {
@@ -26,5 +27,35 @@ export class UserImageService {
       relations: ['user'],
     });
     return result;
+  }
+
+  // Update User Image
+  // @param updateUserImageInput 이미지를 수정할 유저의 id와 url
+  // @returns `userImage`
+  async update({
+    updateUserImageInput,
+  }: {
+    updateUserImageInput: UpdateUserImageInput;
+  }) {
+    const { userId, ...userImage } = updateUserImageInput;
+
+    // 기존 User Image 가져오기
+    const userImageDate = await this.userImageRepository.findOne({
+      where: { user: { id: userId } },
+    });
+
+    if (userImageDate) {
+      // 기존 이미지 삭제하기
+      this.userImageRepository.softDelete({
+        user: { id: userId },
+      });
+      // 새로운 이미지 저장하기
+      const result: UserImage = await this.userImageRepository.save({
+        ...userImage,
+        user: { id: userId },
+        relations: ['user'],
+      });
+      return result;
+    }
   }
 }
